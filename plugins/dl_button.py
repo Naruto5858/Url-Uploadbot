fde578a8bc380cddb8e122ef16e2f0440aeabe6c
Naruto5858 <107269300+Naruto5858@users.noreply.github.com>
2022-07-15 17:32:17 +0530

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | Modifieded By : @DC4_WARRIOR
+# (c) Shrimadhav U K | Modifieded By : @Minato
 
 # the logging things
 import logging
