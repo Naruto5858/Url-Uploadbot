ea254b928b4abe6fb4718a92d0ff7e925146ea86
Naruto5858 <107269300+Naruto5858@users.noreply.github.com>
2022-07-15 09:33:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | Modifieded By : @DC4_WARRIOR
+# (c) Shrimadhav U K | Modifieded By : @Minato
 
 # the logging things
 from pyrogram import filters
-from pyrogram import Client as Clinton
+from pyrogram import Client as Naruto
 import os
 import logging
 logging.basicConfig(level=logging.DEBUG,
@@ -26,10 +26,10 @@ if __name__ == "__main__":
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
-    Warrior = Clinton(
-        "@All_Url_Uploader",
+    Minato = Naruto (
+        "@Url-Uploadbot",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
         plugins=plugins)
-    Warrior.run()
+    Minato.run()
